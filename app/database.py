53fbb32e18d75a599cdad7efdbc152cf364f4433
Kaiky Tupinambá <kaikymoura972@gmail.com>
2025-05-05 13:56:29 -0300

    fix: Fix security issues

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-
-
 from app.models.user import Base
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+asyncpg://postgres:kaikeviss4896@localhost:5432/db_filelens"
-)
+
+load_dotenv(override=True)
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
@@ -20,8 +22,10 @@ async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+
 async def shutdown_db():
     await engine.dispose()
+
 
 async def get_db():
     async with AsyncSessionLocal() as db:
